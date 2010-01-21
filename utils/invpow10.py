4bafddbb044f91b531d374618c4f770abc0008af
Giovanni Bajo <rasky@develer.com>
2010-01-21 17:34:42 +0100

    Move invpow10.py in subdirectory

---
+++ utils/invpow10.py
@@ -0,0 +1,30 @@
+from __future__ import division
+from decimal import *
+
+getcontext().prec = 100
+
+def make_table(bits, num):
+    table = []  
+
+    for i in range(0,num):
+        for j in range(0,100):
+            if Decimal(2**j) / 10**i > 1:
+                break
+        else:
+            raise RuntimeError, v
+        j -= 1
+        v = Decimal(2**j) / 10**i
+        x = long(v * (2**bits))
+        table.append((x,j))
+
+    print "-------------------"
+    for (x,j) in table:
+        print (x >> j) / 2**bits
+
+    print "-------------------"
+    for (x,j) in table:
+        print "%s, %d," % (hex(x),j)
+
+
+make_table(32, 9)
+make_table(64, 19)
