e23330a96f14d9fe5eb91724487bc968b11708e9
Giovanni Bajo <rasky@develer.com>
2010-01-23 15:39:07 +0100

    Fix 128-bits type definition

--- fixedpoint_config.h
+++ fixedpoint_config.h
@@ -3,8 +3,8 @@
 
 #ifdef __x86_64__
     #define FRACT_HAS_128BITS
-    typedef unsigned long long uint128_t;
-    typedef signed long long int128_t;
+    typedef __uint128_t uint128_t;
+    typedef __int128_t long int128_t;
 #endif
 
 // Avoid using any division
